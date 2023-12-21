e7b3ea708f919084f25725ff9477b6a7b37070c4
redbird504 <timurza@icloud.com>
2023-12-21 14:38:11 +0300

    add error handler

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
+from fastapi.responses import JSONResponse
 
 from src.auth.router import router as auth_router
 from src.users.router import router as users_router
 from src.config import app_configs, settings, STATIC_DIR
+from src.common.exceptions import DetailedHTTPException
 
 app = FastAPI(**app_configs)
 
@@ -21,6 +24,20 @@ app.add_middleware(
 @app.get("/healthcheck", include_in_schema=False)
 async def healthcheck() -> dict[str, str]:
     return {"status": "ok"}
+
+
+@app.exception_handler(DetailedHTTPException)
+async def http_exception_handler(request, exc):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content=jsonable_encoder(
+            {
+                "status": False,
+                "msg": exc.detail,
+                "details": {}
+            }
+        ),
+    )
 
 
 app.include_router(auth_router, prefix="/auth", tags=["Auth"])
